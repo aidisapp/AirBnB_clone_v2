26157200054ec07690069025556b6975e07ede6e
Prosper Atu <atu.prosper@gmail.com>
2024-04-15 13:48:40 +0100

    Updated the user test file

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" """
+"""Module for state model test"""
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """
+    Test cases for the state model
+    Inherits from base model test
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """test for the constructor initialization"""
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """test for the name attribute"""
         new = self.value()
         self.assertEqual(type(new.name), str)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-""" """
+"""Module for the user model tests"""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """
+    test cases for the user model
+    inherits from the test base model
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """test for the constructor initialization"""
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """ """
+        """test for first name attribute"""
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """test for last name attribute"""
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """test for email attribute"""
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """test for password attribute"""
         new = self.value()
         self.assertEqual(type(new.password), str)
