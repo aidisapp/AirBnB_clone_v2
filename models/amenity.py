c05afb5ffd460682a506131f1dabe4ee1e8103ef
Prosper Atu <atu.prosper@gmail.com>
2024-04-20 15:59:29 +0100

    finished models

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+State Module for HBNB project
+Defines the Amenities model
+
+"""
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    This is the class for Amenity
+    Attributes:
+        name: input name
+        __tablename__: private attribute name for the  table
+        place_amenities: foreign key to the place model
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary=place_amenity)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+import shlex
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        """Cities object"""
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
